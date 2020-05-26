9d8c0ced389e76c416bc8ba24de555c1c5a2cb20
Tony Johnson <tonyj@stanford.edu>
2020-05-25 20:47:57 -0700

    Now works with CCSWebTrending 2.0

--- ccs_tools/CCSTrending.py
+++ ccs_tools/CCSTrending.py
@@ -1,19 +1,22 @@
-from string import Template
-import fnmatch
 import datetime
-from IPython.core.display import display, Javascript
-import pandas as pd
+import fnmatch
+from string import Template
+
 import numpy as np
+import pandas as pd
 import pytz
 import requests
+from IPython.core.display import Javascript, display
 
 DEFAULT_REST_URL = "https://lsst-camera-dev.slac.stanford.edu/CCSWebTrending/rest/"
 DEFAULT_SITE = "ir2"
+
 
 class Channel:
     ''' Represents a single channel as read from the trending tree. A Channel may
     represent a folder with no actual data, or a leaf node with associated trending
     data '''
+
     def __init__(self, restURL, path, node):
         self.restURL = restURL
         self.path = path
@@ -79,15 +82,16 @@ class Channel:
                 c = Channel(self.restURL, full_path, node)
                 self.children[c.text] = c
 
+
 class ChannelMapHelper:
     def __init__(self, path, cm):
         self.channels = cm.find_all(path, leaf_only=True)
         if not self.channels:
             raise RuntimeError("Path did not match any leaf nodes: "+path)
         elif len(self.channels) == 1:
-            fp = self.channels[0].full_path()
-            self.suggested_title = fp[:-1]
-            self.suggested_names = fp[-1:]
+            matches = self.channels[0].full_path().split('/')
+            self.suggested_title = "/".join(matches[:-1])
+            self.suggested_names = matches[-1:]
         else:
             matches = self.channels[0].full_path().split('/')
             for channel in self.channels[1:]:
@@ -108,9 +112,11 @@ class ChannelMapHelper:
                 names[channel_index] += '/'.join(channel_name)
             self.suggested_names = names
 
+
 class ChannelMap:
     def __init__(self, site=DEFAULT_SITE, restURL=DEFAULT_REST_URL):
-        self.root = Channel(restURL+site+"/channels/", "", {'text': '', 'id': 0, 'children': True})
+        self.root = Channel(restURL+site+"/channels/", "",
+                            {'text': '', 'id': 0, 'children': True})
 
     def find(self, name):
         return self.root.find(name)
@@ -123,6 +129,7 @@ class ChannelMap:
 
     def ls(self, recursive=False):
         return self.root.ls(recursive)
+
 
 class ChannelDataReader:
 
@@ -163,7 +170,8 @@ class TimePeriod:
         elif isinstance(period, tuple):
             return StartEndTimePeriod(period[0], period[1])
         else:
-            raise RuntimeError("Unsupported argument type for for_range "+period)
+            raise RuntimeError(
+                "Unsupported argument type for for_range "+period)
 
 
 class StartEndTimePeriod(TimePeriod):
@@ -175,7 +183,8 @@ class StartEndTimePeriod(TimePeriod):
         return (TimePeriod.to_millis(self.start), TimePeriod.to_millis(self.end))
 
     def as_ccs_string(self):
-        return "{start: new Date(%d), end: new Date(%d)}" % (TimePeriod.to_millis(self.start), TimePeriod.to_millis(self.end))
+        return "{&quot;start&quot;: %d, &quot;end&quot;: %d}" % (TimePeriod.to_millis(self.start), TimePeriod.to_millis(self.end))
+
 
 class DeltaTimePeriod(TimePeriod):
     def __init__(self, delta):
@@ -186,12 +195,13 @@ class DeltaTimePeriod(TimePeriod):
         return (TimePeriod.to_millis(now-self.delta), TimePeriod.to_millis(now))
 
     def as_ccs_string(self):
-        return "%d" % self.delta.total_seconds()
+        return "%d" % (self.delta.total_seconds() * 1000)
+
 
 class CCSTrending:
     """A simple jupyter interface to CCS trending"""
 
-    def __init__(self, title="Trending Plot", data=None, range=datetime.timedelta(days=1), site=DEFAULT_SITE, restURL=DEFAULT_REST_URL):
+    def __init__(self, title=None, data=None, range=datetime.timedelta(days=1), site=DEFAULT_SITE, restURL=DEFAULT_REST_URL):
         self.plots = []
         self.title = title
         self.range = range
@@ -202,8 +212,8 @@ class CCSTrending:
         if data:
             if isinstance(data, str):
                 cmh = ChannelMapHelper(data, self.cm)
-                # TODO: Dont replace user supplied title
-                self.title = cmh.suggested_title
+                # Dont replace user supplied title
+                self.title = self.title if self.title else cmh.suggested_title
                 for channel, name in zip(cmh.channels, cmh.suggested_names):
                     self.add_channel(channel, name)
             elif isinstance(data, dict):
@@ -229,7 +239,8 @@ class CCSTrending:
             channel = self.cm.find(id)
             self.plots.append([channel.data, key if key else channel.text])
         else:
-            raise RuntimeError('Unsuppored argument type for add_channel '+ id)
+            raise RuntimeError(
+                'Unsuppored argument type for add_channel ' + id)
 
     def add_all(self, channels):
         for channel in channels:
@@ -249,56 +260,25 @@ class CCSTrending:
     def plot(self):
         code = Template('''
             (function(element) {
-                 new Promise(function(resolve, reject) {
+                new Promise(function(resolve, reject) {
                     var script = document.createElement("script");
                     script.onload = resolve;
                     script.onerror = reject;
-                    script.src = "//cdnjs.cloudflare.com/ajax/libs/dygraph/1.1.1/dygraph-combined.js";
+                    script.src = "https://lsst-camera-dev.slac.stanford.edu/CCSWebTrending/ccs-trending.js";
+                    script.type = "module";
                     document.head.appendChild(script);
-               }).then(() => {
-                 new Promise(function(resolve, reject) {
-                    var script = document.createElement("script");
-                    script.onload = resolve;
-                    script.onerror = reject;
-                    script.src = "https://lsst-camera-dev.slac.stanford.edu/CCSWebTrending/CCSTrending.js";
-                    document.head.appendChild(script);
-               }).then(() => {
-		$$('<style>.dygraph-legend {left: 70px !important;}</style>').appendTo(element);
-		let div = $$('<div style="width:100%;height:400px"></div>').appendTo(element)
-		let g = new CCSTrendingPlot(
-		        div[0], {
-		    title: '${title}',
-		    restURL: '${restURL}',
-		    range: ${range},
-		    data: { ${data} }
-		});
-		let zoomDict = {'hour': 3600, '3 hour': 3*3600, '6 hour': 6*3600, '12 hour': 12*3600,
-		   'day': 86400, 'week': 604800, 'month': 30 * 86400 };
-		let errorBarDict = { 'none': 'NONE', 'minmax': 'MINMAX', 'rms': 'RMS'}
-		function zoom(event) { g.zoom(event.data); }
-		function setErrorBars(event) { g.setErrorBars(event.data); }
-
-		$$('<b>Zoom:&nbsp;</b>').appendTo(element);
-		for (var key in zoomDict) {
-		  let a = $$('<a href="#">'+key+'</a>').appendTo(element);
-		  a.click(zoomDict[key],zoom);
-		  element.append('&nbsp;');
-		}
-		$$('<b>Error Bars:&nbsp;</b>').appendTo(element);
-		for (var key in errorBarDict) {
-		  let a = $$('<a href="#">'+key+'</a>').appendTo(element);
-		  a.click(errorBarDict[key],setErrorBars);
-		  element.append('&nbsp;');
-		}
-              });});
-	    })(element);
+                }).then(() => {
+		            $$(`<div><trending-controller></trending-controller><trending-plot  style="width:100%;height:300px" title="${title}" restURL="${restURL}" range="${range}">${data}</trending-plot></div>`).appendTo(element);
+                });
+            })(element);
            ''')
+
         def output(plots):
             result = ""
             for id, key in plots:
-                result += "%d: {title: '%s'}," % (id, key)
+                result += '<trending-data key="%d">%s</trending-data>' % (id, key)
             return result
 
-        js = Javascript(code.substitute(title=self.title, data=output(self.plots), range=self.range.as_ccs_string(), restURL=self.restURL))
+        js = Javascript(code.substitute(title=self.title, data=output(
+            self.plots), range=self.range.as_ccs_string(), restURL=self.restURL))
         display(js)
-
